b4fea2161761430a94b76d209485ead16c942100
barto <18618583+rajlu@users.noreply.github.com>
2020-12-21 23:40:53 +0100

    Add tooltips to buttons

--- src/components/utility/icon-button/icon-button.tsx
+++ src/components/utility/icon-button/icon-button.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './icon-button.scss';
 import { TablerIconProps } from '@tabler/icons';
+import Tooltip from '../tooltip';
 
 type IconButtonProps = {
   text: string;
@@ -8,18 +9,22 @@ type IconButtonProps = {
   onClick: () => void;
 };
 
-const IconButton = ({ text, icon, onClick }: IconButtonProps) => (
-  <button
-    className="icon-button p-12 mr-4 xl:mr-24 last:mr-0 flex flex-row rounded-xs transition-all duration-150 hover:bg-black-700"
-    type="button"
-    onClick={onClick}
-  >
-    {icon}
-    <p className="ml-8 hidden xl:block">{text}</p>
-    <div className="tooltip py-4 px-8 rounded-2xl xl:hidden bg-black-600 transition-all">
-      <p>{text}</p>
-    </div>
-  </button>
-);
+const IconButton = ({ text, icon, onClick }: IconButtonProps) => {
+  const [isTooltipsHidden, setIsTooltipsHidden] = React.useState(true);
+
+  return (
+    <button className="icon-button" type="button" onClick={onClick}>
+      <div
+        className="p-12 mr-4 xl:mr-24 last:mr-0 flex flex-row rounded-xs transition-all duration-150 hover:bg-black-700"
+        onMouseEnter={() => setIsTooltipsHidden(false)}
+        onMouseLeave={() => setIsTooltipsHidden(true)}
+      >
+        {icon}
+        <p className="ml-8 hidden xl:block">{text}</p>
+      </div>
+      <Tooltip text={text} hidden={isTooltipsHidden} />
+    </button>
+  );
+};
 
 export default IconButton;
--- src/components/utility/tooltip/tooltip.tsx
+++ src/components/utility/tooltip/tooltip.tsx
@@ -1,13 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { CSSTransition } from 'react-transition-group';
+import './tooltip.scss';
 
 type TooltipProps = {
-  //
+  text: string;
+  hidden: boolean;
 };
 
-class Tooltip extends Component<TooltipProps, any> {
-  public render() {
-    return <div>Tooltip</div>;
-  }
-}
+const Tooltip = ({ text, hidden }: TooltipProps) => {
+  const nodeRef = React.useRef(null);
+
+  return (
+    <CSSTransition
+      nodeRef={nodeRef}
+      in={!hidden}
+      timeout={{ appear: 300, enter: 300, exit: 0 }}
+      classNames="tooltip-transition"
+    >
+      <div ref={nodeRef} className="tooltip py-4 px-8 rounded-2xl bg-black-600 transition-all">
+        <p>{text}</p>
+      </div>
+    </CSSTransition>
+  );
+};
+
+// https://stackoverflow.com/questions/24502898/show-or-hide-element-in-react
 
 export default Tooltip;
