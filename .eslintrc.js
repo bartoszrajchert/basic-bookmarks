d87e6e89db3a3b9bf6c8534f6dacd3190ad85478
barto <18618583+rajlu@users.noreply.github.com>
2020-12-23 14:33:32 +0100

    Add eslint-plugin-react-hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-  extends: ['airbnb-typescript', 'prettier/react'],
+  extends: ['airbnb-typescript', 'prettier/react', 'plugin:react-hooks/recommended'],
   plugins: ['prettier'],
   rules: {
-    "linebreak-style": "off",
+    'linebreak-style': 'off',
     'object-curly-newline': 'off',
   },
   parser: '@typescript-eslint/parser',
