b6300298467f49356e974020c667ce8b6bd7c67a
barto <18618583+rajlu@users.noreply.github.com>
2020-12-24 00:49:00 +0100

    Debounce hook update

--- src/utilities/hooks/use-debounce.tsx
+++ src/utilities/hooks/use-debounce.tsx
@@ -1,17 +1,40 @@
 import { useState, useEffect } from 'react';
 
+/**
+ * Debounce hook by:
+ * @author Gabe Ragland
+ * @url https://dev.to/gabe_ragland/debouncing-with-react-hooks-jci
+ * @param value
+ * @param delay
+ */
 const useDebounce = (value: any, delay: number) => {
+  // State and setters for debounced value
   const [debouncedValue, setDebouncedValue] = useState(value);
 
-  useEffect(() => {
-    const handler = setTimeout(() => {
-      setDebouncedValue(value);
-    }, delay);
+  useEffect(
+    () => {
+      // Set debouncedValue to value (passed in) after the specified delay
+      const handler = setTimeout(() => {
+        setDebouncedValue(value);
+      }, delay);
 
-    return () => {
-      clearTimeout(handler);
-    };
-  }, [value, delay]);
+      // Return a cleanup function that will be called every time ...
+      // ... useEffect is re-called. useEffect will only be re-called ...
+      // ... if value changes (see the inputs array below).
+      // This is how we prevent debouncedValue from changing if value is ...
+      // ... changed within the delay period. Timeout gets cleared and restarted.
+      // To put it in context, if the user is typing within our app's ...
+      // ... search box, we don't want the debouncedValue to update until ...
+      // ... they've stopped typing for more than 500ms.
+      return () => {
+        clearTimeout(handler);
+      };
+    },
+    // Only re-call effect if value changes
+    // You could also add the "delay" var to inputs array if you ...
+    // ... need to be able to change that dynamically.
+    [delay, value],
+  );
 
   return debouncedValue;
 };
