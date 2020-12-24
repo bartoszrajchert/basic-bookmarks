898381d968ad0f75b815ca463b54813231ebe49f
barto <18618583+rajlu@users.noreply.github.com>
2020-12-24 15:47:18 +0100

    Change files structure

--- src/components/moodboard/collection/header/header.test.tsx
+++ src/components/moodboard/collection/header/header.test.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import { shallow } from 'enzyme';
 import Header from './header';
+import EViewType from '../../../../utilities/enums/collection';
 
 describe('Header', () => {
   test('matches snapshot', () => {
-    const wrapper = shallow(<Header />);
+    const wrapper = shallow(
+      <Header collectionViewType={EViewType.large} onTypeChange={() => {}} />,
+    );
     expect(wrapper).toMatchSnapshot();
   });
 });
