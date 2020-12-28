6a8f73c60b85b6bb2aa8959566e5baa1533efc31
barto <18618583+rajlu@users.noreply.github.com>
2020-12-28 23:07:06 +0100

    Convert paragraph into input

--- src/components/utility/search-bar/search-bar.tsx
+++ src/components/utility/search-bar/search-bar.tsx
@@ -5,7 +5,7 @@ import './search-bar.scss';
 const SearchBar = () => (
   <div className="search-bar py-12 px-16 bg-black-900 flex flex-row rounded-2xl">
     <IconSearch size="24" />
-    <p className="opacity-40 pl-8">Search your bookmarks...</p>
+    <input className="opacity-40 pl-8 bg-transparent" value="Search your bookmarks..." onChange={() => {}}/>
   </div>
 );
 
