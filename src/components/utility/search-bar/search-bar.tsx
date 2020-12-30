893326e20351ebbe7bb27f3e68315f302f0123a5
barto <18618583+rajlu@users.noreply.github.com>
2020-12-30 01:13:25 +0100

    Add state to search bar

--- src/components/utility/search-bar/search-bar.tsx
+++ src/components/utility/search-bar/search-bar.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import { IconSearch } from '@tabler/icons';
 import './search-bar.scss';
 
-const SearchBar = () => (
-  <div className="search-bar py-12 px-16 bg-black-900 flex flex-row rounded-2xl">
-    <IconSearch size="24" />
-    <input className="opacity-40 pl-8 bg-transparent" value="Search your bookmarks..." onChange={() => {}}/>
-  </div>
-);
+const SearchBar = () => {
+  const [inputText, setInputText] = React.useState('Search your bookmarks...');
+
+  return (
+    <div className="search-bar py-12 px-16 bg-black-900 flex flex-row rounded-2xl">
+      <IconSearch size="24" />
+      <input
+        className="opacity-40 pl-8 bg-transparent"
+        value={inputText}
+        onChange={(e) => setInputText(e.currentTarget.value)}
+      />
+    </div>
+  );
+};
 
 export default SearchBar;
