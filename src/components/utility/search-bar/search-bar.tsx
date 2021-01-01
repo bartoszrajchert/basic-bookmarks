9166f2403e8e5ff44177c79266fba71c406bfa2c
barto <18618583+rajlu@users.noreply.github.com>
2021-01-01 23:43:34 +0100

    Add placeholder to search bar

--- src/components/utility/search-bar/search-bar.tsx
+++ src/components/utility/search-bar/search-bar.tsx
@@ -3,15 +3,24 @@ import { IconSearch } from '@tabler/icons';
 import './search-bar.scss';
 
 const SearchBar = () => {
-  const [inputText, setInputText] = React.useState('Search your bookmarks...');
+  const placeholderText = 'Search your bookmarks...';
+  const [inputText, setInputText] = React.useState(placeholderText);
+  const [isPlaceholder, setIsPlaceholder] = React.useState(true);
+
+  const changeText = (newText: string, placeholder: boolean) => {
+    setInputText(newText);
+    setIsPlaceholder(placeholder);
+  };
 
   return (
     <div className="search-bar py-12 px-16 bg-black-900 flex flex-row rounded-2xl">
       <IconSearch size="24" />
       <input
-        className="opacity-40 pl-8 bg-transparent"
+        className={[isPlaceholder ? 'opacity-40' : '', 'pl-8 bg-transparent'].join(' ')}
         value={inputText}
-        onChange={(e) => setInputText(e.currentTarget.value)}
+        onChange={(e) => changeText(e.currentTarget.value, false)}
+        onClick={() => (isPlaceholder ? changeText('', false) : null)}
+        onBlur={() => changeText(placeholderText, true)}
       />
     </div>
   );
